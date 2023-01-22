bd4287007a1b90385ba5cb968c79de032b37af82
obhi-d <obi.de.online@gmail.com>
2023-01-22 19:30:49 +0100

    Todo:ocean todo

--- source/Frameworks/Terra/forge/media/shaders/terrain.glsl
+++ source/Frameworks/Terra/forge/media/shaders/terrain.glsl
@@ -88,26 +88,42 @@ const float foamSpeed = 0.05;
 // Author @patriciogv - 2015
 // http://patriciogonzalezvivo.com
 
-vec2 random2(vec2 n) {
-  return vec2(fract(sin(dot(n, vec2(12.9898, 78.233))) * 43758.5453));
-}
+vec3 mod289(vec3 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec2 mod289(vec2 x) { return x - floor(x * (1.0 / 289.0)) * 289.0; }
+vec3 permute(vec3 x) { return mod289(((x*34.0)+1.0)*x); }
 
-float noise(vec2 st) {
-  vec2 i = floor(st);
-  vec2 f = fract(st);
-
-  vec2 u = f * f * (3.0 - 2.0 * f);
-
-  return mix(mix(dot(random2(i + vec2(0.0, 0.0)), f - vec2(0.0, 0.0)),
-                dot(random2(i + vec2(1.0, 0.0)), f - vec2(1.0, 0.0)), u.x),
-              mix(dot(random2(i + vec2(0.0, 1.0)), f - vec2(0.0, 1.0)),
-                dot(random2(i + vec2(1.0, 1.0)), f - vec2(1.0, 1.0)), u.x), u.y);
+float noise(vec2 v) {
+    const vec4 C = vec4(0.211324865405187,  // (3.0-sqrt(3.0))/6.0
+                      0.366025403784439,  // 0.5*(sqrt(3.0)-1.0)
+                      -0.577350269189626,  // -1.0 + 2.0 * C.x
+                      0.024390243902439); // 1.0 / 41.0
+    vec2 i  = floor(v + dot(v, C.yy) );
+    vec2 x0 = v -   i + dot(i, C.xx);
+    vec2 i1;
+    i1 = (x0.x > x0.y) ? vec2(1.0, 0.0) : vec2(0.0, 1.0);
+    vec4 x12 = x0.xyxy + C.xxzz;
+    x12.xy -= i1;
+    i = mod289(i); // Avoid truncation effects in permutation
+    vec3 p = permute( permute( i.y + vec3(0.0, i1.y, 1.0 ))
+                  + i.x + vec3(0.0, i1.x, 1.0 ));
+    vec3 m = max(0.5 - vec3(dot(x0,x0), dot(x12.xy,x12.xy), dot(x12.zw,x12.zw)), 0.0);
+    m = m*m ;
+    m = m*m ;
+    vec3 x = 2.0 * fract(p * C.www) - 1.0;
+    vec3 h = abs(x) - 0.5;
+    vec3 ox = floor(x + 0.5);
+    vec3 a0 = x - ox;
+    m *= 1.79284291400159 - 0.85373472095314 * ( a0*a0 + h*h );
+    vec3 g;
+    g.x  = a0.x  * x0.x  + h.x  * x0.y;
+    g.yz = a0.yz * x12.xz + h.yz * x12.yw;
+    return 130.0 * dot(m, g);
 }
 
 vec3 get_foam(vec2 uv, float depth, float time) {
     // foam intensity
   float foam = smoothstep(0.08, 0.01, depth);
-  foam = foam - (noise(uv + vec2(time * 0.1, 0.0)) + noise(uv + vec2(0.0, time * 0.1))) * 0.01;
+  foam = (foam - (noise(uv + vec2(time * 0.04, 0.0)) + noise(uv + vec2(0.0, time * 0.04)))) * 0.5;
   foam = clamp(foam, 0.0, 1.0) * 0.6;
   vec3 foam_color = vec3(1, 1, 1) * foam;
   return foam_color;
@@ -115,7 +131,7 @@ vec3 get_foam(vec2 uv, float depth, float time) {
 
 vec3 get_waves(vec2 uv, float depth, float time) {
     // Waves
-  float wave_intensity = pow(abs(noise(vec2(uv.x + time * 0.1, uv.y + depth * 4.0 + time * 0.1))), 3.0) * .001;
+  float wave_intensity = pow(abs(noise(vec2(uv.x + time * 0.1, uv.y + depth * 4.0 + time * 0.1))), 3.0) * .1;
   vec3 wave_color = vec3(0.5, 0.5, 1) * wave_intensity;
   return wave_color;
 }
